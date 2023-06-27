6bc7ea3efff3d43db95704b98fc02f8d45f0a9e3
Ale TI <alessandrodev@outlook.com>
2023-06-27 15:32:19 -0300

    commit layout testes

---
+++ app/src/main/java/com/ela/estoquegs/Reader.kt
@@ -0,0 +1,11 @@
+package com.ela.estoquegs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Reader : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reader)
+    }
+}
