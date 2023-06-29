e1a3e5214189c5275660046d45797f41399544b0
Ale TI <alessandrodev@outlook.com>
2023-06-29 16:37:21 -0300

    layout teste inicial

--- app/src/main/java/com/ela/estoquegs/MainActivity.kt
+++ app/src/main/java/com/ela/estoquegs/MainActivity.kt
@@ -2,45 +2,15 @@ package com.ela.estoquegs
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.ela.estoquegs.ui.theme.EstoqueGSTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContent {
-            EstoqueGSTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_reader)
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EstoqueGSTheme {
-        Greeting("Android")
-    }
-}
